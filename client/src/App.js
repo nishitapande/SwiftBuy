e3abcd766b2fdb493f50d15fcf329bdddf1e0fab
Nishita Pande <89330962+nishitapande@users.noreply.github.com>
2024-03-16 22:34:23 +0530

    Added Home Screen
    Created Product Component and created the Home Screen Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Container } from "react-bootstrap";
+import HomeScreen from "./screens/HomeScreen";
 function App() {
   return (
     <>
       <Header />
       <main className="py-3">
         <Container>
-          <h1>THIS IS APP</h1>
+          <HomeScreen />
         </Container>
       </main>
 
